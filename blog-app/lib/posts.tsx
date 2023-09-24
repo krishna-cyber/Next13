7b77f2e09869292f08cdc2317de4ad8d0783693d
krishna-cyber <tiwarikrishna54321@gmail.com>
2023-09-24 19:28:02 +0545

    blogpost data

--- blog-app/lib/posts.tsx
+++ blog-app/lib/posts.tsx
@@ -1,35 +1,38 @@
 /** @format */
+import BlogPost from "@/app/components/BlogPost";
 import fs from "fs";
 import matter from "gray-matter";
 import path from "path";
 
 //getting post directory
-const postDirectory = path.join(process.cwd(), "blogposts");
+const postsDirectory = path.join(process.cwd(), "blogposts");
 
 export function getSortedPosts() {
   //getting all filenames in an array
-  const filenames = fs.readdirSync(postDirectory);
+  const filenames = fs.readdirSync(postsDirectory);
   //const allPostDatas
-  const allPostsData = filenames.map((filename) => {
-    const id = filename.replace(/\.md$/, "");
 
-    //getting fullpath
-    const fullPath = path.join(postDirectory, filename);
-    const fileContent = fs.readFileSync(fullPath, "utf-8");
+  const fileNames = fs.readdirSync(postsDirectory);
+  const allPostsData = fileNames.map((fileName) => {
+    // Remove ".md" from file name to get id
+    const id = fileName.replace(/\.md$/, "");
 
-    //extracting metadata
-    const matterResult = matter(fileContent);
+    // Read markdown file as string
+    const fullPath = path.join(postsDirectory, fileName);
+    const fileContents = fs.readFileSync(fullPath, "utf8");
+
+    // Use gray-matter to parse the post metadata section
+    const matterResult = matter(fileContents);
+
     const blogPost: BlogPost = {
       id,
       title: matterResult.data.title,
       date: matterResult.data.date,
     };
 
-    //combine the data with id
+    // Combine the data with the id
     return blogPost;
   });
-
-  return allPostsData.sort((a, b) => {
-    a.date < b.date ? 1 : -1;
-  });
+  // Sort posts by date
+  return allPostsData.sort((a, b) => (a.date < b.date ? 1 : -1));
 }
